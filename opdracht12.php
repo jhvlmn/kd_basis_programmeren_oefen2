bf385baf5ad701ca48f3f5bb2ace44839fe43afb
Jhvlmn <jeroenheuvelman@outlook.com>
2024-10-02 09:46:57 +0200

    init

--- opdracht12.php
+++ opdracht12.php
@@ -9,6 +9,8 @@ Opdracht 1: Voeg een tweede dobbesteen toe
 Opdracht 2: Zorg ervoor dat er getoond wordt welke waarde er met deze tweede dobbelsteen 2 gegooid is
 Opdracht 3: Tel de waardes van dobbelsteen1 en dobbelsteen2 bij elkaar op en toon de uitkomst op het scherm
 
+Opdracht 4: Pas het programma aan, zodat de gebruiker zelf kan aangeven met hoeveel dobbelstenen hij wil gooien
+
 
 */
 
